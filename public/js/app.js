3d9a77981289b11bff590483ca697e29d3309543
Edoka <edokamezie@gmail.com>
2016-04-21 19:56:55 -0700

    Add support for chat rooms

--- public/js/app.js
+++ public/js/app.js
@@ -2,20 +2,26 @@ var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
 var socket = io();
 
-
+console.log(name + 'joined ' + room);
+var $roomtitle = jQuery('.room-title');
+$roomtitle.append(room);
 socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
-	console.log(name + ' joined ' + room);
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 });
 
 socket.on('message', function(message) {
 	var momentTimestamp = moment.utc(message.timestamp);
+
 	var $message = jQuery('.messages');
-	
+
 	console.log('New message: ');
 	console.log(message.text);
 	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a: ') + '</strong></p>');
-	$message.append('<p>'  + message.text + '</p>');
+	$message.append('<p>' + message.text + '</p>');
 });
 
 //Handles submitting of new message
@@ -23,26 +29,25 @@ var $form = jQuery('#message-form');
 
 $form.on('submit', function(event) {
 	event.preventDefault();
-	
-     
-     // --- My solution
-     // var now = moment();
-// 
-	 // var timestamp = now.valueOf('x');
-	 // var timestampMoment = moment.utc(timestamp);
-	 // var time = timestampMoment.local().format('h:mm ')
+
+	// --- My solution
+	// var now = moment();
+	//
+	// var timestamp = now.valueOf('x');
+	// var timestampMoment = moment.utc(timestamp);
+	// var time = timestampMoment.local().format('h:mm ')
 
 	var $message = $form.find('input[name=message]');
-	
+
 	socket.emit('message', {
 		//--- My solution
 		// text: time + ' ' + $message.val()
-		
-		name: name,
-		text: $message.val()
+
+		name : name,
+		text : $message.val()
 	});
-	
+
 	$message.val('');
-	
+
 });
 
--- server.js
+++ server.js
@@ -7,8 +7,20 @@ var moment  = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
+var clientInfo = {};
+
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
+	
+	socket.on('joinRoom', function(req) {
+		clientInfo[socket.id] = req;
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message', {
+			name: 'System',
+			text: req.name + ' has joined!',
+			timestamp: moment().valueOf()
+		});
+	});
 		
 
 		//My solution
@@ -27,7 +39,7 @@ io.on('connection', function (socket) {
 		 // to send to everybody but not ourself
 		//socket.broadcast.emit('message', message);
 		// io.emit()... to send the message to everbody including ourself
-		io.emit('message', message);
+		io.to(clientInfo[socket.id].room).emit('message', message); //add to() this emit the message only to pple in the room
 	});
 	
 	socket.emit('message', {
